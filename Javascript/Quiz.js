c7e43da7442c12f632254d6df37a0348361d7e9d
King_Aryan <aryanshahbaz86@gmail.com>
2023-02-16 04:30:53 +0330

    1401/11/27
    Quiz L5
    Quiz L6
    menu template

---
+++ Javascript/Quiz.js
@@ -0,0 +1,8 @@
+function Incorrect_answer(){
+    alert("جواب درست : <>")
+  }
+  function Correct_answer(){
+    alert("جواب شما کاملا درست است. :)")
+  }
+
+//   <script src="./Javascript/Quiz.js"></script>
